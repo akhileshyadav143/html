f4e559590991265896fe954d75bb8e94a9430d35
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-03-31 22:11:37 +0530

    html

---
+++ htmlpracties.html/script24.js
@@ -0,0 +1,40 @@
+console.log("loops");
+
+let a=1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(a+i);
+    
+// }
+// let obj={
+//     name:"happy",
+//     role:"programmer",
+//     company: "coder happy"
+// }
+
+// for(const key in obj){
+//     console.log(key)
+// }
+
+// for(const c of "happy"){
+//     console.log(c)
+// }
+
+
+// let i=0;
+// while(i<6000){
+//     console.log(i)
+//     i++;
+// }
+
+
+let i=0;
+do{
+    console.log(i)
+    i++;
+
+}while(i<6);
