2383b05f7b324a8558cc58553d718fc59f084a56
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-04 14:48:56 +0530

    html

---
+++ htmlpracties.html/script25.js
@@ -0,0 +1,23 @@
+function nice(name){
+    console.log("hey" + name + "you are  nice")
+    console.log("hey" + name + " you are good!")
+    console.log("hey" + name + " your tshirt is nice!")
+    console.log("hey" + name + " you course is good too nice!")
+}
+
+// console.log("hey happy you are nice!")
+// console.log(" hey happy you are good!")
+// console.log("hey happy your tshirt is nice!")
+// console.log(" hey happy you course is good too nice!")
+
+// nice("Akhilesh")
+
+// nice("happy")
+
+function sum(a,b){
+    // console.log(a+b)
+     return a+b
+}
+
+result=sum(4,8)
+console.log("The sum of these numbers is:",result)
