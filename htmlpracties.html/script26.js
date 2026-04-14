fe344c5d0ab875113754d9f9609f4088d4eabaca
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-14 23:32:16 +0530

    java

---
+++ htmlpracties.html/script26.js
@@ -0,0 +1,13 @@
+consloe.log(" this is sring ")
+let a="Akhilesh";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length)
+let name="Akhilesh";
+let friend='raj';
+console.log("his name is" + name + "and his friends name is "+friend)
