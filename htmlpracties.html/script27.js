f7e0f29f5437cc1b234f8d0ecac9811b7f60a7ca
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-15 23:38:04 +0530

    js

---
+++ htmlpracties.html/script27.js
@@ -0,0 +1,17 @@
+let arr =[1,2,3,4,6]
+
+console.log(arr)
+console.log(arr.length)
+console.log(arr[0])
+console.log(arr[2])
+console.log(arr[4])
+arr[0]=5666;
+console.log(arr.toString())
+console.log(arr.join("and"))
+
+let newArr =arr.map((e,index,array)=>{
+    return e**2
+})
+
+console.log(newArr)
+
