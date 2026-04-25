f731be86183544a29ee3bc82886e953e0c179ec7
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-25 23:29:47 +0530

    html

---
+++ htmlpracties.html/script30.js
@@ -0,0 +1,12 @@
+console.log("Akhilesh")
+// let RAdha = document.getElementsByClassName("Box")
+// console.log("RAdha")
+
+// RAdha[2].style.backgroundColor="red"
+
+// document.querySelector(".Box").style.backgroundColor = "green";
+console.log(document.querySelectorAll(".Box"))
+
+document.querySelectorAll(".Box").forEach(e =>{
+ e.style.backgroundColor = "green";
+})
