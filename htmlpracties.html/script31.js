b1585d2a610b7b653fc10dd2a147a68c3e37ad98
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-26 23:56:46 +0530

    html

---
+++ htmlpracties.html/script31.js
@@ -0,0 +1,9 @@
+let a= 6
+ function factorial(number){
+    let arr =Array.from(Array(number+1).keys)
+    console.log(arr.slice(1,))
+    arr.slice(1,).reduce(()=>{
+     return a*b
+    })
+ }
+ factorial(a)
