4503c50d657f7ece351c84ca37f747bd72826ee7
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-07 22:41:04 +0530

    js

---
+++ htmlpracties.html/script34.js
@@ -0,0 +1,12 @@
+console.log("happy is hacker")
+console.log("ram is hacker")
+setTimeout(() =>{
+    console.log(" I am isside settimeout") 
+},0);
+
+
+
+setTimeout(() =>{
+    console.log(" I am isside settimeout") 
+},0);
+console.log("THe End")
